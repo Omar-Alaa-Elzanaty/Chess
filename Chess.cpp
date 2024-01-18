3736675059af89732d9950047ebb6ed547b27f9a
Omar Alaa <Omaralaaelzanaty@gmail.com>
2024-01-18 21:27:21 +0200

    Setup Architecture

---
+++ Chess.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+#include "System.h"
+using namespace std;
+int main()
+{
+	System::Run();
+}
---
+++ System.h
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class System
+{
+
+public:
+	static void Run() {
+		cout << "Hello";
+	}
+
+};
+
