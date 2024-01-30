04c1bf40cf37b932aecb39a134a84ffe87db4f3c
omar elsayed <138625022+omarEls2yed@users.noreply.github.com>
2024-01-30 21:56:24 +0200

    Update Chess.cpp

--- Chess.cpp
+++ Chess.cpp
@@ -31,10 +31,9 @@ using namespace std;
 int main()
 {
     Board* b = new Board;
-    for (int i = 0; i < 8; i++)
-    {
-        for (int j = 0; j < 8; j++)
-            cout << b->board[i][j] << ' ';
-        cout << endl;
-    }
+    Piece* p = new Piece;
+    p = b->board[8][2];
+    KnightService x;
+    Board xx = x.AllValidMove(b, p);
+    BoardServices().DisplayBoard(&xx);
 }
