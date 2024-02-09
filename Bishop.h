ebcf7bed3b14e3f8ca123107ba77c318ea39e24f
Eslam Ayman <solooom.ayman.2003@gmail.com>
2024-02-09 04:21:13 +0200

    Directions from array to vector

--- Bishop.h
+++ Bishop.h
@@ -1,36 +1,13 @@
 #pragma once
 #include "Piece.h"
-#include <algorithm>
-#include <array>
-#include <bitset>
-#include <cassert>
-#include <chrono>
-#include <climits>
-#include <cmath>
-#include <complex>
-#include <cstring>
-#include <functional>
-#include <iomanip>
-#include <iostream>
-#include <map>
-#include <numeric>
-#include <queue>
-#include <random>
-#include <set>
-#include <vector>
-#include <stack>
-#include <unordered_set>
-#include <unordered_map>
-#include <algorithm>
-#include <fstream>
-#include <bitset>
+
 using namespace std;
 class Bishop :public Piece
 {
 public:
 	Bishop() {
-		this->dRow = new int[4];
-		this->dColumn = new int[4];
+		this->dRow.resize(4);
+		this->dColumn.resize(4);
 		dRow[0] = 1; dRow[1] = 1;
 		dRow[2] = -1; dRow[3] = -1;
 
