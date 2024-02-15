7688df97d3a32034c39b39fc3894f4d3c5f81c95
omar <om87867@gmail.com>
2024-02-15 21:35:45 +0200

    update

--- Bishop.h
+++ Bishop.h
@@ -29,8 +29,8 @@ class Bishop :public Piece
 {
 public:
 	Bishop() {
-		this->dRow = new int[4];
-		this->dColumn = new int[4];
+		this->dRow.resize(4);
+		this->dColumn.resize(4);
 		dRow[0] = 1; dRow[1] = 1;
 		dRow[2] = -1; dRow[3] = -1;
 
