74a6f0b3c6601db6ec2e7f216ca36a074268945d
heba islam <hebatolla2t@gmail.com>
2024-02-17 03:31:05 +0200

    fixed the bug in Pawn and Rook services

--- Chess.cpp
+++ Chess.cpp
@@ -28,17 +28,9 @@
 #include"Knight.h"
 #include"IKnightService.h"
 #include "RookServices.h"
+#include "PawnServices.h"
 using namespace std;
 int main()
 {
-	Board* b = new Board;
-	Piece* p = new Piece;
-	p = b->board[8][2];
-	KnightService x;
-	Board xx = x.AllValidMove(b,p);
-	BoardServices().DisplayBoard(&xx);
-	cout << endl;
-	cout << x.CanMove(b,p) << endl;
-	Board xu = x.MakeMove(6,3,b,p);
-	BoardServices().DisplayBoard(&xu);
+	
 }
